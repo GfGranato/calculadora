7f285d0c65f485959f040cc8565c6bed3ebbd704
Guilherme.Granato <guilherme.granato@ab-inbev.com>
2022-06-29 20:29:24 -0300

    first commit

---
+++ src/br/com/cod3r/calc/visao/Display.java
@@ -0,0 +1,19 @@
+package br.com.cod3r.calc.visao;
+
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class Display extends JPanel {
+
+    private final JLabel label;
+    public Display() {
+        setBackground(new Color(46, 49, 50));
+        label = new JLabel("123");
+        label.setBackground(Color.WHITE);
+        label.setFont(new Font("courier",Font.PLAIN,30));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+        add(label);
+    }
+}
---
+++ src/br/com/cod3r/calc/visao/Teclado.java
@@ -0,0 +1,11 @@
+package br.com.cod3r.calc.visao;
+
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class Teclado extends JPanel {
+    public Teclado() {
+        setBackground(Color.RED);
+    }
+}
